8e33eb7dfbcad76fb7992c12c9fe6d423fd1cc4b
Marta Castrillo <mart.castrillo@gmail.com>
2023-05-30 17:31:41 +0200

    add rest spread

--- 04-rest-spread.js
+++ 04-rest-spread.js
@@ -15,3 +15,22 @@ let { username, age } = user;
 console.log(username, age);
 // Opcion 2
 console.log(username, user.age);
+
+
+/// spread operator
+
+let person = {name: "Marta", age: 35 }
+let country = 'MX'
+
+let data = {id:1, ...person, country};
+console.log(data);
+
+// rest
+
+function sum(num, ...values) {
+    console.log(values);
+    console.log(num + values[0]);
+    return num + values[0];
+}
+
+sum(1, 1, 2, 3)
